86f523eac7f8c30f4d8b7b238cee334ead32d477
mauro.di.dio@accenture.com <mauro.di.dio@accenture.com>
2014-06-23 15:21:37 +0200

    Corrected indentation

--- TestGit/src/com/testgit/TestGit.java
+++ TestGit/src/com/testgit/TestGit.java
@@ -1,7 +1,7 @@
 package com.testgit;
 
 public class TestGit {
-public static void main(String[] args) {
-	System.out.println("TEST");
-}
+	public static void main(String[] args) {
+		System.out.println("TEST");
+	}
 }
